3baa4cb0aec909d0e1ab44c23461996162b07503
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-06-09 21:34:00 -0700

    dropdowns added with class

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
 
-// import needed modules
 let cityBuild = getDefaultCity();
 
 function getDefaultCity() {
@@ -17,6 +16,17 @@ const cityNameInput = sectionCityInfo.querySelector('input');
 
 const sectionCityDesign = document.getElementById('section-city-design');
 const header = sectionCityDesign.querySelector('h2');
+
+const environmentSelect = sectionCityInfo.querySelector('select');
+
+const selectEnvironment = document.getElementById('select-environment');
+const environmentImage = document.getElementById('environment-image');
+let environmentCounter = 0;
+
+const architectureSelect = document.getElementById('architecture-select');
+const architectureImage = document.getElementById('architecture-image');
+let architectureCounter = 0;
+
 // components
     // component
     // define and grab DOM elements
@@ -24,17 +34,38 @@ const header = sectionCityDesign.querySelector('h2');
 
 function displayCityInfo() {
     cityNameInput.value = cityBuild.name;
+    environmentSelect.value = cityBuild.environment;
 }
 
 function displayCity() {
     sectionCityDesign.classList.value = '';
     header.textContent = cityBuild.name;
+    sectionCityDesign.classList.add(cityBuild.environment);
 }
+
     // optional: subscribe to events
         // event handlers - what needs to happen?
+
 cityNameInput.addEventListener('input', () => {
     cityBuild.name = cityNameInput.value;
     displayCity();
+});
+
+environmentSelect.addEventListener('change', () => {
+    cityBuild.environment = environmentSelect.value;
+    displayCity();
+});
+
+selectEnvironment.addEventListener('change', (e) => {
+    const value = e.target.value;
+    environmentImage.src = `./assets/enron-${value}.jpeg`;
+    environmentCounter;
+});
+
+architectureSelect.addEventListener('change', (e) => {
+    const value = e.target.value;
+    architectureImage.src = `./assets/arch-${value}.jpeg`;
+    architectureCounter;
 });
 
 console.log('h2');
